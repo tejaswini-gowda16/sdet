e070dd259d54acd74a276e094e06eb40c2375995
tejaswini-gowda16 <77953794+tejaswini-gowda16@users.noreply.github.com>
2021-02-05 08:57:14 +0530

    Add files via upload

---
+++ python/Activity4.py
@@ -0,0 +1,39 @@
+
+while True:
+    player1 = input("Enter your choice - ROCK/PAPER/SCISSORS: ").upper()
+    player2 = input("Enter your choice - ROCK/PAPER/SCISSORS: ").upper()
+
+    if player1 == player2:
+        print ("It's a tie, both player chose the same option!")
+    elif(player1=='ROCK'):
+        if(player2=='PAPER'):
+            print ("Player2 wins! PAPER covers ROCK!")
+        elif(player2=='SCISSORS'):
+            print ("Player1 wins! ROCK crushes SCISSORS!")
+        else:
+            print("Invalid input! Enter only ROCK/PAPER/SCISSORS. Try again.")
+    elif(player1=='PAPER'):
+        if(player2=='SCISSORS'):
+            print ("Player2 wins! SCISSORS cut PAPER!")
+        elif(player2=='ROCK'):
+            print ("Player1 wins! PAPER covers ROCK!")
+        else:
+            print("Invalid input! Enter only ROCK/PAPER/SCISSORS. Try again.")
+    elif(player1=='SCISSORS'):
+        if(player2=='ROCK'):
+            print ("Player2 wins! ROCK crushes SCISSORS!")
+        elif(player2=='PAPER'):
+            print ("Player1 wins! SCISSORS cut PAPER!")
+        else:
+            print("Invalid input! Enter only ROCK/PAPER/SCISSORS. Try again.")
+    else:	
+        print("Invalid input! Enter only ROCK/PAPER/SCISSORS. Try again.")
+    
+    again = input ("Do you wish to play again? (Y/N): ").upper()
+    if again=='Y':
+        pass
+    elif again=='N':
+        raise SystemExit
+    else:
+	    print("You entered neither Y/N. Exiting now.")
+	    raise SystemExit
