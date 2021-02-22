e0728d74db5ad6abca31e99cd75ebb5034b9a51b
tejaswini-gowda16 <77953794+tejaswini-gowda16@users.noreply.github.com>
2021-02-22 13:35:10 +0530

    Add files via upload

---
+++ TestNG/Activity4_DemoTwo.java
@@ -0,0 +1,9 @@
+package testNGTests;
+import org.testng.annotations.Test;
+
+public class Activity4_DemoTwo {
+    @Test
+    public void TestCase() {
+        System.out.println("I'm in the test case from DemoTwo Class");
+    }
+}
