3d257f5cfcc7f1cdc6064f31b2a5c2be63af6dc6
tejaswini-gowda16 <77953794+tejaswini-gowda16@users.noreply.github.com>
2021-02-28 11:30:05 +0530

    Add files via upload

---
+++ API_Project/API_Activity_1b.groovy
@@ -0,0 +1,35 @@
+package Groovy_Practices
+
+class API_Activity_1b {
+
+	static void main (def args) {
+		File file = new File("src/Groovy_Practices/file")
+		file.createNewFile();
+
+		file.write("A quick brown Fox jumped over the lazy Cow\n")
+		file.append("John Jimbo jingeled happily ever after\n")
+		file.append("Th1\$ 1\$ v3ry c0nfus1ng")
+
+		file.eachLine {
+			if (it =~ /Cow$/)
+			{
+				println it
+			}
+			if (it =~/^J/)
+			{
+				println it
+			}
+			if (it =~/\d{2}/)
+			{
+				println it
+			}
+		}
+
+		def m1 = file.text =~/\S+er/
+		println m1.findAll()
+
+		def m2 = file.text =~/\S*\d\W/
+		println m2.findAll()
+	}
+}
+
