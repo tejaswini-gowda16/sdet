7ce536fa81ceacab55f738c666e9089fe23a9db1
tejaswini-gowda16 <77953794+tejaswini-gowda16@users.noreply.github.com>
2021-02-28 12:50:52 +0530

    Add files via upload

---
+++ API_Project/API_Activity_1_a.groovy
@@ -0,0 +1,15 @@
+package Groovy_Practices
+
+class API_Activity_1_a {
+	static void main (def args) {
+		//heterogeneous list
+		def list = [11, 2, 19, 5, "Mango", "Apple", "Watermelon"]
+		
+		def List1 = list.minus([11, 2, 19, 5]);
+		println ("Sorted String list: " + List1.sort());
+		
+		def List2 = list.minus(["Mango", "Apple", "Watermelon"]);
+		println ("Sorted Integer list: " + List2.sort());
+			
+	}
+}
